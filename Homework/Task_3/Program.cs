6d4ae9bcadc70c8a150780c963fd31f8177eaaab
Volodya <volodya.atoyan@list.ru>
2022-12-21 23:51:13 +0300

    Added Task_3

---
+++ Homework/Task_3/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+Clear();
+Write("Введите число ");
+int a = Convert.ToInt32(ReadLine());
+
+if(a%2 == 0)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
