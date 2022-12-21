7103807ea13e4194f276858a7639875bf66a7376
Volodya <volodya.atoyan@list.ru>
2022-12-21 22:37:12 +0300

    Added Task_1

---
+++ Homework/Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+Clear();
+Write("Введите число а ");
+int a = Convert.ToInt32(ReadLine());
+Write("Введите число b ");
+int b = Convert.ToInt32(ReadLine());
+
+if (a > b)
+{
+    WriteLine($"max = {a}");
+    WriteLine($"min = {b}");
+}
+else
+{
+    WriteLine($"max = {b}");
+    WriteLine($"min = {a}");
+}
+
