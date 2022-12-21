55b4b007be5cd2cfa148246b8cd599f550b955cc
Volodya <volodya.atoyan@list.ru>
2022-12-21 23:26:57 +0300

    Added Task_2

---
+++ Homework/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+Clear();
+Write("Введите число а ");
+int a = Convert.ToInt32(ReadLine());
+Write("Введите число b ");
+int b = Convert.ToInt32(ReadLine());
+Write("Введите число c ");
+int c = Convert.ToInt32(ReadLine());
+int max = a;
+
+if (b > a)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+WriteLine(max);
