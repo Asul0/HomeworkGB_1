2ffc6b8b1a0f5e6e028fa6760db133b054ca8e19
Volodya <volodya.atoyan@list.ru>
2022-12-22 14:29:38 +0300

    Added Task_4

---
+++ Homework/Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+Clear();
+Write("Введите число ");
+int number = Convert.ToInt32(ReadLine());
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
